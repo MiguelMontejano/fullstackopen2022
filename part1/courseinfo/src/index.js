d4fdfc4ae8bbe2f2745e4d32587fed525b83ee2c
Miguel Montejano <miguelmontejanoprofesional@gmail.com>
2022-02-03 20:29:21 +0100

    Ejercicios 1.3-1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -39,8 +39,10 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -54,12 +56,14 @@ const App = () => {
       exercises: 14
     }
   ]
+}
+
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
